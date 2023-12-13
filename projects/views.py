ef28983b3ac3de189d5c8cb2bc9b8910e2b01421
Daniel <daniel.gomes.9@hotmail.com>
2023-12-13 16:11:39 -0300

    resolve requisito 4

--- projects/views.py
+++ projects/views.py
@@ -4,6 +4,7 @@ from .models import Profile, Project
 from .serializers import ProfileSerializer, ProjectSerializer
 from rest_framework.permissions import (
     IsAuthenticated,
+    AllowAny,
 )
 
 
@@ -11,6 +12,23 @@ class ProfileViewSet(viewsets.ModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
     permission_classes = [IsAuthenticated]
+
+    def get_permissions(self):
+        if self.request.method == "GET":
+            return [AllowAny()]
+        return [IsAuthenticated()]
+
+    def retrieve(self, request, *args, **kwargs):
+        if request.method == "GET":
+            profile_id = kwargs.get("pk")
+            profile = Profile.objects.get(id=profile_id)
+            # Crie um contexto com dados que você deseja passar para o template
+            context = {
+                "profile": profile,
+            }
+            # Renderize o template
+            return render(request, "profile_detail.html", context)
+        return super().retrieve(request, *args, **kwargs)
 
 
 class ProjectViewSet(viewsets.ModelViewSet):
